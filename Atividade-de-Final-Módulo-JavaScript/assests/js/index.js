c8ba1c4394885e5b25933af326378d9bfaa553cc
RogerioFsilva <rog.fernandes@yahoo.com.br>
2022-09-20 22:09:40 -0300

    Revisão Final 20-09

---
+++ Atividade-de-Final-Módulo-JavaScript/assests/js/index.js
@@ -0,0 +1,38 @@
+"use strict";
+if (localStorage.getItem('usuarioLogado')) {
+    alert('Você já está logado!');
+    window.location.href = 'messages.html';
+}
+const btnEntrar = document.getElementById('btnEntrar');
+btnEntrar.addEventListener('click', (e) => {
+    e.preventDefault();
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let log = buscarDados();
+    let verificarUsuario = log.findIndex((usuario) => usuario.email === email.value && usuario.senha === password.value);
+    console.log(verificarUsuario);
+    if (verificarUsuario >= 0) {
+        const usuarioLogado = {
+            email: log[verificarUsuario].email,
+            messages: log[verificarUsuario].messages,
+        };
+        salvarDados(usuarioLogado);
+        setTimeout(() => {
+            window.location.href = 'messages.html';
+        }, 1000);
+    }
+    else {
+        alert('Cadastre-se primeiro.');
+    }
+});
+//arquivos com ignore para que eu possa usar as funções de forma duplicada.
+// @ts-ignore
+function salvarDados(usuarios) {
+    localStorage.setItem('usuarioLogado', JSON.stringify(usuarios));
+}
+//arquivos com ignore para que eu possa usar as funções de forma duplicada.
+// @ts-ignore
+function buscarDados() {
+    const usuarios = JSON.parse(localStorage.getItem('usuarios') || '[]');
+    return usuarios;
+}
